0afd24593a97f746053f2a0024f7b9e5d9e6ed46
izabela <iza.kalenik@gmail.com>
2024-01-09 21:23:02 +0100

    addded loyalt points to account

---
+++ BookWorm/Controllers/LoyaltyPointsController.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Core;
+using BookWorm.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BookWorm.Controllers
+{
+    public class LoyaltyPointsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.LoyaltyPoints = 0;
+
+            return View();
+        }
+
+    }
+}
